2f5a95a75aa51d57b38c814b5337f8d189e68afd
Domantas Jocas <domantas0216@gmail.com>
2026-02-17 21:47:41 +0200

    Stack operations

--- bootsector.asm
+++ bootsector.asm
@@ -1,15 +1,28 @@
-[org 0x7c00]
+[org 0x7c00] ;Sets origin to 0x7c00, where the BIOS loads the boot sector
 
 
-mov ah, 0x0e
-mov al, 'H'
+mov ah, 0x0e ;Set the teletype output function for BIOS interrupts
+mov bp, 0x8000
+mov sp, bp
+
+push 'A'
+push 'B'
+push 'C'
+
+pop bx
+mov al, bl
 int 0x10
-mov al, [the_secret]
-int 0x10
+
+pop bx
+mov al, bl
 int 0x10
 
-the_secret:
-    db 'E'
+mov al , [0x7ffe]
+int 0x10
 
-times 510-($-$$) db 0
-dw 0xaa55
+jmp $
+
+
+
+times 510-($-$$) db 0 ;Fill the rest of the boot sector with zeros until we reach 510 bytes
+dw 0xaa55 ;Last two bytes of the boot sector must be 0xAA55 to indicate a valid boot sector
